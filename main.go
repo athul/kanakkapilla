1e7d091fb2c2b2771e31cd0daf23e685353de2f6
Athul Cyriac Ajay <athul8720@gmail.com>
2021-01-08 19:32:45 +0530

    Moved to Postgres and Updated a Bunch of stuff

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 	"net/http"
 
 	"github.com/jmoiron/sqlx"
-	_ "github.com/mattn/go-sqlite3"
+	_ "github.com/lib/pq"
 )
 
 // DB Schema of Sqlite is
@@ -19,21 +19,24 @@ type Transaction struct {
 	ID          int            `db:"id"`
 	Tdate       string         `db:"tdate"`
 	Date        string         `db:"date"`
-	Description string         `db:"desc"`
+	Description string         `db:"description"`
 	Refno       sql.NullString `db:"ref"`
 	Debit       sql.NullString `db:"debit"`
 	Credit      sql.NullString `db:"credit"`
-	Balance     sql.NullString `db:"bal"`
+	Balance     float64        `db:"bal"`
 }
-type upiTransactions struct {
-	ID          int            `db:"id"`
-	Date        string         `db:"date"`
-	Description string         `db:"desc"`
-	Refno       sql.NullString `db:"ref"`
-	Debit       sql.NullString `db:"debit"`
-	Credit      sql.NullString `db:"credit"`
-	Balance     sql.NullString `db:"bal"`
-	Total       int
+
+// AllData saves all the Data from the CSV File.
+type AllData struct {
+	// Save all the Transcations from the account
+	AllTrans []Transaction
+	// Saves all the UPI transactions from the account
+	UPITrans  []Transaction
+	upiNos    int
+	upiDebAm  float64
+	CurBal    float64
+	BalonDate string
+	maxUpitrs int
 }
 
 var (
@@ -42,60 +45,54 @@ var (
 )
 
 func main() {
-	db, err = sqlx.Open("sqlite3", "bank.db")
+	db, err = sqlx.Connect("postgres", "")
 	if err != nil {
 		log.Println(err)
 	}
 	trans := []Transaction{}
 
-	if err = db.Select(&trans, `SELECT * FROM bank ORDER BY date ASC`); err != nil {
+	if err = db.Select(&trans, `SELECT * FROM bank ORDER BY id ASC`); err != nil {
 		log.Println(err)
 	}
-	// for _, t := range trans {
-	// 	log.Println(t.Credit, "\n----")
-	// 	log.Println("\n", t.Debit)
-	// }
-	// log.Println("----------")
-	sumDebits()
-	getBalance()
-	getUPI()
-	http.HandleFunc("/", renderTemplate)
+
+	sumDebitsfromUPI()
+	upiTrans := getUPI()
+	all := AllData{
+		AllTrans:  trans,
+		CurBal:    trans[len(trans)-1].Balance,
+		BalonDate: trans[len(trans)-1].Date,
+		UPITrans:  upiTrans,
+	}
+	http.HandleFunc("/", all.renderTemplate)
 	http.ListenAndServe(":8080", nil)
 }
-func renderTemplate(h http.ResponseWriter, r *http.Request) {
+func (a *AllData) renderTemplate(h http.ResponseWriter, r *http.Request) {
 	temp, err := template.New("index.html").ParseFiles("index.html")
 	if err != nil {
 		log.Println(err)
 	}
-	tr := getUPI()
-	if err := temp.Execute(h, tr); err != nil {
+	// tr := getUPI()
+	if err := temp.Execute(h, &a); err != nil {
 		log.Println(err)
 	}
 
 }
-func getUPI() []upiTransactions {
-	upiTrans := []upiTransactions{}
-	if err = db.Select(&upiTrans, `SELECT id,date,desc,ref,debit,credit,bal FROM bank WHERE desc LIKE '%-UPI%' ORDER BY id DESC`); err != nil {
+func getUPI() []Transaction {
+	upiTrans := []Transaction{}
+	if err = db.Select(&upiTrans, `SELECT * FROM bank WHERE description LIKE '%-UPI%' ORDER BY id DESC`); err != nil {
 		log.Println("Unable to fetch UP Transactions", err)
 	}
-	log.Println(len(upiTrans))
 	return upiTrans
 }
 
-func sumDebits() {
-	var sum []float32
-	if err = db.Select(&sum, `SELECT SUM(debit) FROM bank`); err != nil {
+func sumDebitsfromUPI() {
+	var sum []float64
+	if err = db.Select(&sum, `SELECT SUM(debit) FROM bank WHERE description LIKE '%-UPI%'`); err != nil {
 		log.Println("Unable to fetch UP Sum", err)
 	}
 	log.Println("Total Debited Amount", sum[0])
 }
 
-// getBalance gets the current bank balance.
-// It assumes that the balance is at the last row of the table
-func getBalance() {
-	var balance []string
-	if err = db.Select(&balance, `SELECT bal FROM bank ORDER BY id DESC LIMIT 1`); err != nil {
-		log.Println("Unable to fetch UP Sum", err)
-	}
-	log.Println("Current Balance\t:", balance[0])
+func (t *AllData) getMinMaxupi() {
+
 }
